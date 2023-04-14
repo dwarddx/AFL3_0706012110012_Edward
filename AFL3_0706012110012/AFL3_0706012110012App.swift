a98f77ff9d96621c4faf3b03b342014b26d48b3a
dwarddx <92904860+dwarddx@users.noreply.github.com>
2023-04-14 08:45:32 +0700

    Initial Commit

---
+++ AFL3_0706012110012/AFL3_0706012110012App.swift
@@ -0,0 +1,17 @@
+//
+//  AFL3_0706012110012App.swift
+//  AFL3_0706012110012
+//
+//  Created by MacBook Pro on 14/04/23.
+//
+
+import SwiftUI
+
+@main
+struct AFL3_0706012110012App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AFL3_0706012110012/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AFL3_0706012110012
+//
+//  Created by MacBook Pro on 14/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
