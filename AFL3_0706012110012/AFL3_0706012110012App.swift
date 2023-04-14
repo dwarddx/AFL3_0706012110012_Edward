6e15bf49f7fd6e0c29cf7617c19ad4dff1ddf7ac
dwarddx <92904860+dwarddx@users.noreply.github.com>
2023-04-14 10:48:17 +0700

    Update

--- AFL3_0706012110012/AFL3_0706012110012App.swift
+++ AFL3_0706012110012/AFL3_0706012110012App.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct AFL3_0706012110012App: App {
+    @StateObject private var modelData = ModelData()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(modelData)
         }
     }
 }
---
+++ AFL3_0706012110012/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  AFL3_0706012110012
+//
+//  Created by MacBook Pro on 14/04/23.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
